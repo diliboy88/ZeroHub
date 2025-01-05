b902c6eb642727815d3bd23d6661583370c75af3
Dylan Corea <developrpi@gmail.com>
2025-01-05 20:20:07 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function learnMore() {
+  alert("The <link>ZeroWire Hub</link> is designed to revolutionize the way you connect and interact with your <link>Raspberry Pi</link> and other devices. Stay tuned for more updates!");
+}
